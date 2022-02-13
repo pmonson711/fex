05606228b56562f7ee0c2c6a6cae2911897bd6e6
Paul Monson <4119175+pmonson711@users.noreply.github.com>
2022-02-13 16:57:12 -0600

    Minimal interface for predicate

---
+++ compiler/predicate.mli
@@ -0,0 +1,11 @@
+type 'a key = [ `Key of 'a ]
+type 'a value = [ `Value of [ `String of 'a ] ]
+type 'a pair = [ `Pair of 'a key * 'a value ]
+
+val filter_to_predicate :
+     match_fun:('a -> 'a Ast.match_operation -> bool)
+  -> 'a Ast.t
+  -> 'a pair
+  -> bool
+
+val pair_of_strings : 'a -> 'a -> 'a pair
